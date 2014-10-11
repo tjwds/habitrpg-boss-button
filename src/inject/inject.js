e121a30a71ec0fba2cb6daa05466b4ca1428065d
Joe Woods <woodsja@vcu.edu>
2014-10-11 14:19:24 -0400

    0.0.1
    This is the extension that is very obviously poorly coded and made with
    extensionizr

---
+++ src/inject/inject.js
@@ -0,0 +1,39 @@
+chrome.extension.sendMessage({}, function(response) {
+	var readyStateCheckInterval = setInterval(function() {
+	if (document.readyState === "complete") {
+		clearInterval(readyStateCheckInterval);
+
+		// ----------------------------------------------------------
+		// This part of the script triggers when page is done loading
+		//console.log("Hello. This message was sent from scripts/inject.js");
+		// ----------------------------------------------------------
+
+/*function bossbutton(){
+	if ($("#undefined-sticky-wrapper").is(":visible") == true){
+		$("undefined-sticky-wrapper").hide();
+		$(".toolbar-wallet").hide();
+		$(".rewards").hide();
+	} else {
+		$("undefined-sticky-wrapper").show();
+		$(".toolbar-wallet").show();
+		$(".rewards").show();
+	}
+}*/
+
+$("<li id=\"bossbutton\">boss button</li>" ).insertAfter( $( ".toolbar-settings" ) );
+$("#bossbutton").click(
+	function() {
+		if ($("#undefined-sticky-wrapper").is(":visible") == true){
+			$("#undefined-sticky-wrapper").hide();
+			$(".toolbar-wallet").hide();
+			$(".rewards").hide();
+		} else {
+			$("#undefined-sticky-wrapper").show();
+			$(".toolbar-wallet").show();
+			$(".rewards").show();
+		}
+	}
+)
+	}
+	}, 10);
+});
